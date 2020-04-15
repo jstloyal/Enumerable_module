4acd59708a6dd2b531adc735bd57d1414ccfad56
jstloyal <jstloyalty@gmail.com>
2020-04-15 14:46:37 +0100

    add return when no block given to each_index method

--- enumerable.rb
+++ enumerable.rb
@@ -10,14 +10,19 @@ module Enumerable
     self
   end
 
-  def my_each_with_index(x, y)
-    return empty? unless block_given?
-    x = is_a?(Range) ? to_a : self
+  def my_each_with_index
+    return enum_for unless block_given?
+
+    arr = is_a?(Range) ? to_a : self
+   
     i = 0
-    while i < x.length
-      yield(x[i])
+    while i < arr.length
+      yield(arr[i], i)
       i += 1
     end
+    self
   end
+
+  
 end
 
--- test.rb
+++ test.rb
@@ -1,6 +1,10 @@
 require_relative 'enumerable'
-[2,3,4,5,7].my_each
-puts "-------------"
-(1..5).my_each { |x| puts x }
-puts "----------"
-(3..8).each_with_index { |x| puts x * 2 }
+# [2,3,4,5,7].my_each
+# puts "-------------"
+# (1..5).my_each { |x| puts x }
+ "----------"
+# (3..8).my_each_with_index { |x, i| "#{x}, #{i}" } 
+
+# a = [2,3,4,2,6,7]
+# p a.my_count 
+
