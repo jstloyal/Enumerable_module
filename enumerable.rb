f8bc5aafab98503213eeff2544d9a868edbc6047
Ioannis Kousis <bonobo@otenet.gr>
2020-04-30 13:27:13 +0300

    Implemented rspec tests for my_none, my_map and my_inject methods

--- enumerable.rb
+++ enumerable.rb
@@ -8,7 +8,6 @@ module Enumerable
       yield(arr[i])
       i += 1
     end
-    
   end
 
   def my_each_with_index
@@ -26,7 +25,7 @@ module Enumerable
   def my_count(*args)
     if args.count.positive?
       counter = 0
-      my_each { |item| c += 1 if item == args[0] }
+      my_each { |item| counter += 1 if item == args[0] }
       return counter
     end
     return length unless block_given?
@@ -56,12 +55,12 @@ module Enumerable
 
   def my_all?(*args)
     arr = is_a?(Range) ? to_a : self
-    if args.count.positive? 
+    if args.count.positive?
       if args[0].class.name == 'Regexp'
         my_each { |item| return false if (item =~ args[0]).nil? }
       elsif args[0].is_a?(Class)
         my_each { |item| return false unless item.is_a?(args[0]) }
-      else args[0].is_a?(Object)
+      elsif args[0].is_a?(Object)
         my_each { |item| return false unless item == args[0] }
       end
       return true
@@ -70,6 +69,7 @@ module Enumerable
       i = 0
       while i < arr.size
         return false if arr[i] == false || arr[i].nil?
+
         i += 1
       end
       return true
@@ -85,12 +85,12 @@ module Enumerable
 
   def my_any?(*args)
     arr = is_a?(Range) ? to_a : self
-    if args.count.positive? 
+    if args.count.positive?
       if args[0].class.name == 'Regexp'
         my_each { |item| return true if item =~ args[0] }
       elsif args[0].is_a?(Class)
         my_each { |item| return true if item.is_a?(args[0]) }
-      else args[0].is_a?(Object)
+      elsif args[0].is_a?(Object)
         my_each { |item| return true if item == args[0] }
       end
       return false
@@ -100,6 +100,7 @@ module Enumerable
       i = 0
       while i < arr.size
         return true if arr[i] != false && !arr[i].nil?
+
         i += 1
       end
       return false
@@ -110,17 +111,17 @@ module Enumerable
       return true if yield(arr[i])
 
       i += 1
-    end   
+    end
     false
   end
 
   def my_none?(*args)
-    if args.count.positive? 
+    if args.count.positive?
       if args[0].class.name == 'Regexp'
-        my_each { |item| return false if !(item =~ args[0]).nil? }
+        my_each { |item| return false unless (item =~ args[0]).nil? }
       elsif args[0].is_a?(Class)
         my_each { |item| return false if item.is_a?(args[0]) }
-      else args[0].is_a?(Object)
+      elsif args[0].is_a?(Object)
         my_each { |item| return false if item == args[0] }
       end
       return true
@@ -139,6 +140,7 @@ module Enumerable
     new_array = []
     if proc.nil?
       return enum_for unless block_given?
+
       my_each { |item| new_array << yield(item) }
     else
       my_each { |item| new_array << proc.call(item) }
@@ -148,7 +150,7 @@ module Enumerable
 
   def my_inject(*args)
     arr = is_a?(Range) ? to_a : self
-    if args.count == 0
+    if args.count.zero?
       total = arr[0]
       i = 1
       while i < arr.size
@@ -157,13 +159,13 @@ module Enumerable
       end
     elsif args.count == 1
       if args[0].is_a?(Numeric)
-       total = args[0]
-       i = 0
-       while i < arr.size
-        total = yield(total, arr[i])
-        i += 1
-       end
-      else args[0].is_a?(Symbol) || args[0].is_a?(String)
+        total = args[0]
+        i = 0
+        while i < arr.size
+          total = yield(total, arr[i])
+          i += 1
+        end
+      elsif args[0].is_a?(Symbol) || args[0].is_a?(String)
         action = args[0].is_a?(String) ? args[0].to_sym : args[0]
         total = arr[0]
         i = 1
@@ -174,7 +176,7 @@ module Enumerable
       end
     elsif args.count == 2
       total = args[0]
-      action = args[1].is_a?(String) ? args[1].to_sym : args[1]   
+      action = args[1].is_a?(String) ? args[1].to_sym : args[1]
       i = 0
       while i < arr.size
         total = total.send(action, arr[i])
@@ -182,5 +184,5 @@ module Enumerable
       end
     end
     total
-	end
+  end
 end
--- multiply_els.rb
+++ multiply_els.rb
@@ -4,4 +4,4 @@ def multiply_els(arr)
   arr.my_inject(:*)
 end
 
-puts multiply_els([2,4, 5])
+puts multiply_els([2, 4, 5])
